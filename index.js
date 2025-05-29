4fdb276ee5973548df2766f58964d447ea04cc5d
Kirill <49115928+kirill-efimov-sk@users.noreply.github.com>
2025-05-29 23:27:12 +0300

    fix: added counter value check

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const init = (initialCount, delay) => {
-    console.time();
-    console.log('The script has been successfully launched!')
-    let counter = initialCount;
+    console.time('timer');
 
+    if(initialCount <= 0) {
+        console.warn('Abort script. InitialCount must be greater than 0');
+        return;
+    };
+    let counter = initialCount;
+    console.log('The script has been successfully launched!');
+    
     const intervalId = setInterval(() => {
         console.log(`${counter}...`);
         counter--;
         
         if (counter === 0) {
-            console.log('The script completed successfully!');
             clearInterval(intervalId);
-            console.timeEnd();
+            console.log('The script completed successfully!');
+            console.timeEnd('timer');
         }
     }, delay);
 } 
 
-init(3, 200)
+init(3, 200);
